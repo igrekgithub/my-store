f7a2275c1226a713276317702a8980731e72f91a
FullStack Y <fullstack@gmail.com>
2022-11-13 00:57:48 +0500

    style Element

--- is.js
+++ is.js
@@ -1,3 +1,6 @@
+const h1 =document.querySelector('h1')
+console.log(h1)
 const newEl = document.createElement('div')
 newEl.innerHTML = `<span  class="git-span">Всем Привет!!!!</span>`
-document.body.innerHTML = `<span  class="git-span">Всем Привет!</span>`
+console.log(newEl)
+h1.after(newEl)
