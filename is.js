d46613ff37f033b8fd494a8f36d1f023c2e20522
FullStack Y <fullstack@gmail.com>
2022-11-13 00:24:37 +0500

    first commit

---
+++ is.js
@@ -0,0 +1,3 @@
+const newEl = document.createElement('div')
+newEl.innerHTML = `<span  class="git-span">Всем Привет!!!!</span>`
+document.body.innerHTML = `<span  class="git-span">Всем Привет!</span>`
