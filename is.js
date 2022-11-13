5c7b42bf4033883cd666b5e2c2ea1596b781c68c
FullStack Y <fullstack@gmail.com>
2022-11-13 08:56:19 +0500

    Проба с gitignore

--- is.js
+++ is.js
@@ -5,3 +5,10 @@ newEl.innerHTML = `<span  class="git-span">Всем Привет!!!!</span>`
 console.log(newEl)
 h1.after(newEl)
 const newElem = document.createElement('ul')
+newElem.insertAdjacent = (
+'afterbegin',
+`<li></li>
+ <li></li>
+ <li></li>
+ <li></li>`)
+console.log(newElem)
