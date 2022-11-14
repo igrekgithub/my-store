87aabf4ef0e9d95ded0037169fcfed0b3a4fe8e5
igrekgithub <ivshome.mybox@gmail.com>
2022-11-14 08:26:49 +0500

    point correction

--- is.js
+++ is.js
@@ -7,8 +7,8 @@ h1.after(newEl)
 const newElem = document.createElement('ul')
 newElem.innerHTML =
 `<li>Пункт №1</li>
- <li>Пункт №12</li>
- <li>Пункт №13</li>
- <li>Пункт №14</li>`
+ <li>Пункт №1</li>
+ <li>Пункт №1</li>
+ <li>Пункт №1</li>`
  newElem.classList.add('roster')
 console.log(newElem)
